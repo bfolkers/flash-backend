b46c75835881f8fc44b58a2e71f721aff5cb3763
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-03-22 13:59:09 -0600

    fixed favorites route

--- routes/favorite.js
+++ routes/favorite.js
@@ -23,7 +23,7 @@ router.post('/', function(req, res){
 
   knex('favorite').insert({
     username_email: req.body.email,
-    deck_id: knex('deck').where('id', req.body.id).select('id'),
+    deck_id: req.body.id,
   }, 'id').then(function(result){
     res.json(result);
   });
