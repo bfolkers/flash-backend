4c944cb5fec77c9ffc85cf0ac9e72a75e6334c9b
Jeff Miguel Hernandez <jeffhernandez89@gmail.com>
2017-03-23 11:29:00 -0600

    deck post now works probably

--- routes/deck.js
+++ routes/deck.js
@@ -3,9 +3,7 @@ const knex = require('../db/knex');
 
 router.get('/', function(req, res) {
   knex('deck')
-  .join('flashcard', 'deck.id', '=', 'flashcard.deck_id')
-  .join('subject', 'subject.id', '=', 'deck.subject_id')
-  .select('subject.name as subject', 'deck.username_email as email', 'flashcard.front', 'flashcard.back', 'deck.name', 'deck.id as id')
+  .select()
   .then(function (result) {
     res.json(result);
   });
